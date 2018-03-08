533a32065d5719814229602cc65a53a82ba401ef
tejc999999 <tejc999999@gmail.com>
2018-03-09 01:14:43 +0900

    課題5 MVC追記

--- src/main/java/jp/te4a/spring/boot/mybootapp4/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/mybootapp4/HelloController.java
@@ -1,13 +1,27 @@
 package jp.te4a.spring.boot.mybootapp4;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
-        return "this is Spring Boot sample.";
+    public String index(Model model) {
+        model.addAttribute("msg", "this is  a setting message");
+        return "index";
     }
+    
+    @RequestMapping(value="/post", method=RequestMethod.POST)
+    public ModelAndView postForm(@RequestParam("text1") String text1) {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("msg", "you write '" + text1 + "'!!!");
+        return mv;
+    }
+
 }
